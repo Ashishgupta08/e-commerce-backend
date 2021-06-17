08591883c5ed599994f499e6369832de61088a88
Ashish Gupta <59931129+Ashishgupta08@users.noreply.github.com>
2021-06-18 01:37:54 +0530

    folder&files created

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+const URI = process.env['URI'];
+const PORT = process.env['PORT'];
+app.use(cors());
+app.use(express.json());
+
+const client = mongoose.connect(URI,{useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=>console.log("\n <===== Connected =====> \n"))
+    .catch(e=>console.log(" \n Could not connect to database. \n Error :- ", e));
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the server of 100 Steps - ECommerce App.")
+});
+
+app.use('*', (req, res) => {
+    res.status(404).send("Error 404 - Page not found.")
+});
+
+app.listen(PORT, () => {
+    console.log(`\n Server Started at port no - ${PORT}`);
+});
